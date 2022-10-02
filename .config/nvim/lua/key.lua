52008b5c37062553be28fc6189108b4fc99e901b
shunsuke6 <sawabes@ns.aucnet.co.jp>
2022-10-02 20:58:42 +0900

    modify dont yank

--- .config/nvim/lua/key.lua
+++ .config/nvim/lua/key.lua
@@ -12,24 +12,28 @@ end
 
 -- No highlight.
 api.nvim_set_keymap("n", "<Leader>h", ":noh<CR>", { noremap = true, silent = true })
-api.nvim_set_keymap("n", "あ", "a" , { noremap = true , silent = true })
-api.nvim_set_keymap("n", "い", "i" , { noremap = true , silent = true })
-api.nvim_set_keymap("n", "う", "u" , { noremap = true , silent = true })
-api.nvim_set_keymap("n", "Ｕ", "U" , { noremap = true , silent = true })
-api.nvim_set_keymap("n", "え", "e" , { noremap = true , silent = true })
-api.nvim_set_keymap("n", "お", "o" , { noremap = true , silent = true })
-api.nvim_set_keymap("n", "O", "O" , { noremap = true , silent = true })
-api.nvim_set_keymap("n", "ｄｄ", "dd" , { noremap = true , silent = true })
-api.nvim_set_keymap("n", "ｙｙ", "yy" , { noremap = true , silent = true })
-api.nvim_set_keymap("n", "ｐ", "p" , { noremap = true , silent = true })
-api.nvim_set_keymap("n", "Ｐ", "P" , { noremap = true , silent = true })
-api.nvim_set_keymap("n", "ｎ", "n" , { noremap = true , silent = true })
-api.nvim_set_keymap("n", "Ｎ", "N" , { noremap = true , silent = true })
--- insert mode C ''
-api.nvim_set_keymap("i", "<C-j>", "<down>" , { noremap = true , silent = true })
-api.nvim_set_keymap("i", "<C-k>", "<up>" , { noremap = true , silent = true })
-api.nvim_set_keymap("i", "<C-h>", "<left>" , { noremap = true , silent = true })
-api.nvim_set_keymap("i", "<C-l>", "<right>" , { noremap = true , silent = true })
-api.nvim_set_keymap("i", "<C-x>", "<delete>" , { noremap = true , silent = true })
 
+-- Japanese
+api.nvim_set_keymap("n", "あ", "a", { noremap = true, silent = true })
+api.nvim_set_keymap("n", "い", "i", { noremap = true, silent = true })
+api.nvim_set_keymap("n", "う", "u", { noremap = true, silent = true })
+api.nvim_set_keymap("n", "Ｕ", "U", { noremap = true, silent = true })
+api.nvim_set_keymap("n", "え", "e", { noremap = true, silent = true })
+api.nvim_set_keymap("n", "お", "o", { noremap = true, silent = true })
+api.nvim_set_keymap("n", "O", "O", { noremap = true, silent = true })
+api.nvim_set_keymap("n", "ｄｄ", "dd", { noremap = true, silent = true })
+api.nvim_set_keymap("n", "ｙｙ", "yy", { noremap = true, silent = true })
+api.nvim_set_keymap("n", "ｐ", "p", { noremap = true, silent = true })
+api.nvim_set_keymap("n", "Ｐ", "P", { noremap = true, silent = true })
+api.nvim_set_keymap("n", "ｎ", "n", { noremap = true, silent = true })
+api.nvim_set_keymap("n", "Ｎ", "N", { noremap = true, silent = true })
 
+-- Insert mode C ''
+api.nvim_set_keymap("i", "<C-j>", "<down>", { noremap = true, silent = true })
+api.nvim_set_keymap("i", "<C-k>", "<up>", { noremap = true, silent = true })
+api.nvim_set_keymap("i", "<C-h>", "<left>", { noremap = true, silent = true })
+api.nvim_set_keymap("i", "<C-l>", "<right>", { noremap = true, silent = true })
+api.nvim_set_keymap("i", "<C-x>", "<delete>", { noremap = true, silent = true })
+
+-- Don't yank
+api.nvim_set_keymap("n", "x", '"_x', { noremap = true })
