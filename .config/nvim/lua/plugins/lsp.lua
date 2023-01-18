8e37b8d32a0eb246fcdb0647907b724effee0770
shunsuke6 <sawabes@ns.aucnet.co.jp>
2023-01-19 01:37:32 +0900

    refactor: delete unwanted comments

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -55,7 +55,6 @@ m.get_capabilities = function()
 end
 
 local function get_clang_capabilities()
-    -- NOTE: Warning will be issued if encoding is not specified.
     local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
     capabilities.offsetEncoding = { "utf-16" }
     return capabilities
@@ -237,24 +236,6 @@ m.setup_lsp_color = function()
 end
 
 m.setup_trouble = function()
-    -- Key mappings by default.
-    -- close = "q",                 close the list
-    -- cancel = "<esc>",            cancel the preview and get back to your last window / buffer / cursor
-    -- refresh = "r",               manually refresh
-    -- jump = { "<cr>", "<tab>" },  jump to the diagnostic or open / close folds
-    -- open_split = { "<c-x>" },    open buffer in new split
-    -- open_vsplit = { "<c-v>" },   open buffer in new vsplit
-    -- open_tab = { "<c-t>" },      open buffer in new tab
-    -- jump_close = { "o" },        jump to the diagnostic and close the list
-    -- toggle_mode = "m",           toggle between "workspace" and "document" diagnostics mode
-    -- toggle_preview = "P",        toggle auto_preview
-    -- hover = "K",                 opens a small popup with the full multiline message
-    -- preview = "p",               preview the diagnostic location
-    -- close_folds = { "zM", "zm" },close all folds
-    -- open_folds = { "zR", "zr" }, open all folds
-    -- toggle_fold = { "zA", "za" },toggle fold of current file
-    -- previous = "k",              preview item
-    -- next = "j"                   next item
     require("trouble").setup({
         height = 10,
         auto_close = true,
