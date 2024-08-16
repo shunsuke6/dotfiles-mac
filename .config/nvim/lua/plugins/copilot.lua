047be36f13458296905c318051dfa224393f0a1c
shunsuke6 <snsk.syk.gtobzz@gmail.com>
2024-08-16 17:06:34 +0900

    fix: Fix copilopt keymap settings

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -2,13 +2,17 @@ local m = {}
 
 m = {
     "github/copilot.vim",
+    lazy = false,
     cmd = { "Copilot" },
-    keys = {
-        { mode = "i", "<C-t>", 'copilot#Accept("<CR>")', { silent = true, expr = true } },
-    },
-    init = function()
-        vim.g.copilot_no_tab_map = true
-    end,
+    -- keys = {
+    --     { mode = "i", "<C-t>", 'copilot#Accept("\\<CR>")', { silent = true, expr = true } },
+    -- },
+    config = function()
+      vim.cmd([[
+        imap <silent><script><expr> <C-t> copilot#Accept("\<CR>")
+        let g:copilot_no_tab_map = v:true
+      ]])
+    end
 }
 
 return m
