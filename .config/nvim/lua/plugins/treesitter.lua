c3a6c565bb615e25efe17d02e9f175d21131fece
shunsuke6 <snsk.syk.gtobzz@gmail.com>
2025-08-05 16:30:44 +0900

    fix: fix style lazy

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,141 @@
 local m = {}
 
-m = {
-    {
+local treesitter = {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })
+    end,
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = "all",
+            sync_install = true,
+            highlight = {
+                enable = true,
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<CR>",
+                    node_incremental = "<CR>",
+                    scope_incremental = "<TAB>",
+                    node_decremental = "<S-TAB>",
+                },
+            },
+            indent = {
+                enable = not vim.g.vscode,
+            },
+            textobjects = {
+                select = {
+                    enable = true,
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                        ["aB"] = "@block.outer",
+                        ["iB"] = "@block.inner",
+                        ["ai"] = "@conditional.outer",
+                        ["ii"] = "@conditional.inner",
+                        ["al"] = "@loop.outer",
+                        ["il"] = "@loop.inner",
+                        ["ap"] = "@parameter.outer",
+                        ["ip"] = "@parameter.inner",
+                        -- ["aF"] = "@frame.outer",
+                        -- ["iF"] = "@frame.inner",
+                        -- ["aS"] = "@statement.outer",
+                        -- ["iS"] = "@scopename.inner",
+                        -- ["a"] = "@comment.outer",
+                        -- ["a"] = "@call.outer",
+                        -- ["i"] = "@call.inner",
+                    },
+                },
+                swap = {
+                    enable = true,
+                    swap_next = {
+                        ["<leader>sn"] = "@parameter.inner",
+                    },
+                    swap_previous = {
+                        ["<leader>sp"] = "@parameter.inner",
+                    },
+                },
+                move = {
+                    enable = true,
+                    goto_next_start = {
+                        ["]m"] = "@function.outer",
+                        ["]]"] = "@class.outer",
+                    },
+                    goto_next_end = {
+                        ["]M"] = "@function.outer",
+                        ["]["] = "@class.outer",
+                    },
+                    goto_previous_start = {
+                        ["[m"] = "@function.outer",
+                        ["[["] = "@class.outer",
+                    },
+                    goto_previous_end = {
+                        ["[M"] = "@function.outer",
+                        ["[]"] = "@class.outer",
+                    },
+                },
+                lsp_interop = {
+                    enable = false,
+                    peek_definition_code = {
+                        ["<leader>lf"] = "@function.outer",
+                        ["<leader>lc"] = "@class.outer",
+                    },
+                },
+            },
+            textsubjects = {
+                enable = true,
+                prev_selection = ",",
+                keymaps = {
+                    ["."] = "textsubjects-smart",
+                    [";"] = "textsubjects-container-outer",
+                    ["i;"] = "textsubjects-container-inner",
+                },
+            },
+            matchup = {
+                enable = false,
+            },
+        })
+    end,
+}
+
+local context = {
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+        require("treesitter-context").setup({
+            enable = true,
+        })
+    end,
+}
+local context_vt = {
+    "haringsrob/nvim_context_vt",
+    config = function()
+        require("nvim_context_vt").setup({
+            enabled = true,
+            disable_virtual_lines = true,
+        })
+    end,
+}
+local hlargs = {
+    "m-demare/hlargs.nvim",
+    dependencies = {
         "nvim-treesitter/nvim-treesitter",
-        build = function()
-            require("nvim-treesitter.install").update({ with_sync = true })
-        end,
     },
+    config = function()
+        require("hlargs").setup()
+    end,
+}
+
+local autotag = {
+    "windwp/nvim-ts-autotag",
+    config = function()
+        require("nvim-ts-autotag").setup()
+    end,
+}
+m = {
+    treesitter,
     { "nvim-treesitter/nvim-treesitter-textobjects" },
     { "RRethy/nvim-treesitter-textsubjects" },
     { "mfussenegger/nvim-ts-hint-textobject" },
@@ -14,16 +143,10 @@ m = {
     { "mizlan/iswap.nvim" },
     { "JoosepAlviste/nvim-ts-context-commentstring" },
     { "andymass/vim-matchup" },
-
-    { "nvim-treesitter/nvim-treesitter-context" },
-    { "haringsrob/nvim_context_vt" },
-    {
-        "m-demare/hlargs.nvim",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-        },
-    },
-    { "windwp/nvim-ts-autotag" },
+    context,
+    context_vt,
+    hlargs,
+    autotag,
     {
         "HiPhish/rainbow-delimiters.nvim",
         config = function()
@@ -42,134 +165,7 @@ m = {
             })
         end,
     },
-
-    config = function()
-        setup_treesitter()
-        setup_context()
-        setup_context_vt()
-        setup_hlargs()
-        setup_autotag()
-    end,
 }
-
-setup_treesitter = function()
-    require("nvim-treesitter.configs").setup({
-        ensure_installed = "all",
-        sync_install = true,
-        highlight = {
-            enable = true,
-        },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<CR>",
-                node_incremental = "<CR>",
-                scope_incremental = "<TAB>",
-                node_decremental = "<S-TAB>",
-            },
-        },
-        indent = {
-            enable = not vim.g.vscode,
-        },
-        textobjects = {
-            select = {
-                enable = true,
-                keymaps = {
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-                    ["ac"] = "@class.outer",
-                    ["ic"] = "@class.inner",
-                    ["aB"] = "@block.outer",
-                    ["iB"] = "@block.inner",
-                    ["ai"] = "@conditional.outer",
-                    ["ii"] = "@conditional.inner",
-                    ["al"] = "@loop.outer",
-                    ["il"] = "@loop.inner",
-                    ["ap"] = "@parameter.outer",
-                    ["ip"] = "@parameter.inner",
-                    -- ["aF"] = "@frame.outer",
-                    -- ["iF"] = "@frame.inner",
-                    -- ["aS"] = "@statement.outer",
-                    -- ["iS"] = "@scopename.inner",
-                    -- ["a"] = "@comment.outer",
-                    -- ["a"] = "@call.outer",
-                    -- ["i"] = "@call.inner",
-                },
-            },
-            swap = {
-                enable = true,
-                swap_next = {
-                    ["<leader>sn"] = "@parameter.inner",
-                },
-                swap_previous = {
-                    ["<leader>sp"] = "@parameter.inner",
-                },
-            },
-            move = {
-                enable = true,
-                goto_next_start = {
-                    ["]m"] = "@function.outer",
-                    ["]]"] = "@class.outer",
-                },
-                goto_next_end = {
-                    ["]M"] = "@function.outer",
-                    ["]["] = "@class.outer",
-                },
-                goto_previous_start = {
-                    ["[m"] = "@function.outer",
-                    ["[["] = "@class.outer",
-                },
-                goto_previous_end = {
-                    ["[M"] = "@function.outer",
-                    ["[]"] = "@class.outer",
-                },
-            },
-            lsp_interop = {
-                enable = false,
-                peek_definition_code = {
-                    ["<leader>lf"] = "@function.outer",
-                    ["<leader>lc"] = "@class.outer",
-                },
-            },
-        },
-        textsubjects = {
-            enable = true,
-            prev_selection = ",",
-            keymaps = {
-                ["."] = "textsubjects-smart",
-                [";"] = "textsubjects-container-outer",
-                ["i;"] = "textsubjects-container-inner",
-            },
-        },
-        matchup = {
-            enable = false,
-        },
-        autotag = {
-            enable = not vim.g.vscode,
-        },
-    })
-end
-
-setup_context = function()
-    require("treesitter-context").setup({
-        enable = true,
-    })
-end
-
-setup_context_vt = function()
-    require("nvim_context_vt").setup({
-        enabled = true,
-        disable_virtual_lines = true,
-    })
-end
-
-setup_autotag = function()
-    require("nvim-ts-autotag").setup()
-end
-
-setup_hlargs = function()
-    require("hlargs").setup()
-end
 
 vim.api.nvim_set_keymap("x", "iu", [[:lua require('treesitter-unit').select()<CR>]], { noremap = true })
 vim.api.nvim_set_keymap("x", "au", [[:lua require('treesitter-unit').select(true)<CR>]], { noremap = true })
