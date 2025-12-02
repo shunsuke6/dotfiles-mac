e81ee192f9f24e60c2c31f6f3a9eb5948688d7f3
shunsuke6 <snsk.syk.gtobzz@gmail.com>
2025-12-03 02:43:37 +0900

    feat: update chat model

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -1,34 +1,36 @@
 local m = {}
 
 m = {
-    "olimorris/codecompanion.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    opts = {
-        strategies = {
-            chat = {
-                name = "copilot",
-                model = "gpt-4.1",
-            },
-        },
-        opts = {
-            log_level = "DEBUG",
-        },
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		strategies = {
+			chat = {
+				adapter = {
+					name = "copilot",
+					model = "claude-sonnet-4.5",
+				},
+			},
+		},
+		opts = {
+			log_level = "DEBUG",
+		},
 
-        -- TODO: setup acp
-        -- adapters = {
-        --     acp = {
-        --         gemini_cli = function()
-        --             return require("codecompanion.adapters").extend("gemini_cli", {
-        --                 env = {
-        --                     api_key = "",
-        --                 },
-        --             })
-        --         end,
-        --     },
-        -- },
-    },
+		-- TODO: setup acp
+		-- adapters = {
+		--     acp = {
+		--         gemini_cli = function()
+		--             return require("codecompanion.adapters").extend("gemini_cli", {
+		--                 env = {
+		--                     api_key = "",
+		--                 },
+		--             })
+		--         end,
+		--     },
+		-- },
+	},
 }
 
 return m
