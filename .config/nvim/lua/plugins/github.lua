f8a7abb32fdf9337582847c96b29d52fb49c0bb7
shunsuke6 <snsk.syk.gtobzz@gmail.com>
2026-08-12 09:00:42 +0900

    fix(nvim): update octo file panel icon option

--- .config/nvim/lua/plugins/github.lua
+++ .config/nvim/lua/plugins/github.lua
@@ -13,7 +13,7 @@ setup_octo = function()
         snippet_context_lines = 4, -- number or lines around commented lines
         file_panel = {
             size = 10, -- changed files panel rows
-            use_icons = true, -- use web-devicons in file panel
+            icons = true, -- use web-devicons in file panel
         },
         mappings = {
             issue = {
